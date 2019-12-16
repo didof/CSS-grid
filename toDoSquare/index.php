83f0ed50483cacf374b1afe6e7307bcc267cab4d
prostagma-code <didonato.fr@gmail.com>
2019-12-16 16:11:44 +0100

    update: new features

--- toDoSquare/index.php
+++ toDoSquare/index.php
@@ -7,7 +7,10 @@
     <title>Document</title>
     <style>
         body {
-            background: #fbe8a6;
+            background: #8590aa ;
+        }
+        ::placeholder{
+            color: white;
         }
         .container{
             display: grid;
@@ -15,16 +18,16 @@
             grid-gap: 1em;
         }
         .container > div {
-            background-color: #f4976c;
+            background-color: #c4dbf6;
             border-radius: 4px;
         }
         input {
             width: 100%;
             padding: 1em 20px;
-            margin: 1em 0;
+            margin: 4px 3px;
             box-sizing: border-box;
             border-bottom: 2px solid blue; border-radius: 4px;
-            background-color: #303c6c;
+            background-color: #e7e3d4;
         }
         input[type=text]:hover {
             background-color: #b4dfe5;
@@ -42,13 +45,14 @@
             grid-gap: 1em;
         }
         .book {
+            height: 100%;
             border-radius: 5px;
             background-color: #b4dfe5;
             margin: 0.5em;
             padding: 3px;
             display: grid;
             grid-template-columns: 1fr 1fr 1fr;
-            grid-template-rows: 1fr 2fr 1fr 4fr 1fr 1fr;
+            grid-template-rows: 1fr 2fr 1fr 3fr 1fr 1fr 1fr;
         }
         .book:nth-child(odd){
             background-color: #d2fdff;
@@ -123,6 +127,24 @@
             grid-row: 6 / 7;
             justify-self: center;
         }
+        .book > .category {
+            text-align: right;
+            font-family: 'Verdana';
+            font-size: 6px;
+            color: black;
+            grid-column: 3 / 4;
+            grid-row: 3 / 4;
+        }
+        .book > .progress {
+            grid-column: 1 / 4;
+            grid-row: 7 / 8;
+        }
+        progress{
+            width: 100%;
+            height: 20px;
+            border-radius: 3px;
+            box-shadow: 0 2px 3px 0;
+        }
         .showBook {
             display: grid;
             grid-template-columns: 2fr 1fr 1fr;
@@ -140,21 +162,45 @@
 </head>
 <body>
 <?php require_once "process.php" ?>
-
 <?php
     $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
     $result = $mysqli->query("SELECT * FROM shelf");
 ?>
 
+<?php 
+    // function bookCounter() {
+    //     $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
+    //     $result = $mysqli->query("SELECT id FROM shelf");
+    //     $array = $result->fetch_array();
+    //     $quantity = count($array);
+    //     return $quantity;
+    // }
+?>
+    
     <div class="container">
         <div class="form">
         <form action="process.php" method="post">
             <input type="hidden" name="id" value="<?php echo $id?>">
             <input type="text" name="name" placeholder="Book's name" value="<?php echo $name ?>">
             <input type="text" name="author" placeholder="Author" value="<?php echo $author ?>">
-            <input type="text" name="date" placeholder="Publication date" value="<?php echo $date ?>">
+            <input type="date" name="date" placeholder="Publication date" value="<?php echo $date ?>">
             <input type="text" name="collection" placeholder="Collection" value="<?php echo $collection ?>">
             <input type="textarea" name="plot" placeholder="Plot" value="<?php echo $plot ?>">
+            <input type="text" name="category" placeholder="Category" value="<?php echo $category ?>">
+            <select name="color">
+                <option value="#fff" autofocus>White</option>
+                <option value="#ffd700">Gold</option>
+                <option value="#c0c0c0">Silver</option>
+                <option value="#f78888">Pink</option>
+                <option value="#f3d250">Yellow</option>
+                <option value="#ececec">Grigio</option>
+                <option value="#90ccf4">Azzurro</option>
+                <option value="#5da2d5">Blu</option>
+                <option value="#f78888">Pink</option>
+                <option value="#000">Black</option>
+            </select>
+            <input type="range" name="range" min="0" max="100" value="1">
+            <hr>
             
             <?php if(!$update): ?>
             <button type="submit" name="save">Put on the shelf.</button>
@@ -166,42 +212,45 @@
         </div>
         <div class="shelf">
             <?php while($book = $result->fetch_assoc()): ?>
-                <div class="book">
+                <div class="book" style="background-color:<?php echo $book['color'] ?>">
                     <div class="collection"><?php echo $book['collection']; ?></div>
                     <div class="name"><?php echo $book['name']; ?></div>
                     <div class="author"><?php echo $book['author']; ?></div>
+                    <div class="category"><?php echo $book['category']; ?></div>
                     <div class="plot"><?php echo $book['plot']; ?></div>
                     <div class="date"><?php echo $book['date']; ?></div>
                     <div class="delete"><a href="index.php?delete=<?=$book['id']?>">Delete</a></div>
                     <div class="edit"><a href="index.php?edit=<?=$book['id']?>">Edit</a></div>
                     <div class="read"><a href="index.php?read=<?=$book['id']?>">Read</a></div>
+                    <div class="progress"><progress max="100" value="<?php echo $book['progress'] ?>"></progress></div>
                 </div>
             <?php endwhile; ?>
         </div>
+        
         <div class="showBook">
-                <?php
-                if(isset($_GET['read'])){
-                    $id = $_GET['read'];
-                    $onSpot = $mysqli->query("SELECT * FROM shelf WHERE id=$id") or die($mysqli->error);
-                while($show = $onSpot->fetch_assoc()): ?>
-                <p id="showTitle">Title: <?php echo $show['name']; ?></p>
-                <p>Author: <?php echo $show['author']; ?></p>
-                <p>Collection: <?php echo $show['collection']; ?></p>
-                <p>Publication date: <?php echo $show['date']; ?></p>
-                <p class="showPlot">Plot: <?php echo $show['plot']; ?></p>
-                <?php endwhile; }?>
+        <?php
+        if(isset($_GET['read'])){
+            $id = $_GET['read'];
+            $onSpot = $mysqli->query("SELECT * FROM shelf WHERE id=$id") or die($mysqli->error);
+        while($show = $onSpot->fetch_assoc()): ?>
+        <p id="showTitle">Title: <?php echo $show['name']; ?></p>
+        <p>Author: <?php echo $show['author']; ?></p>
+        <p>Collection: <?php echo $show['collection']; ?></p>
+        <p>Publication date: <?php echo $show['date']; ?></p>
+        <p class="showPlot">Plot: <?php echo $show['plot']; ?></p>
+        <?php endwhile; }?>
         </div>
     </div>
 </body>
 <!--
 da implementare:
+-contatore libri
+-svuota shelf
 -grafica decente
 -messaggistica
--drop box
 -sort
 -no collection -> titolo
 -fix book size
--fix data type
 
 -->
 </html>
--- toDoSquare/process.php
+++ toDoSquare/process.php
@@ -1,11 +1,15 @@
 <?php
-$id= 0;
-$name= "";
-$author= "";
-$date= "";
-$collection= "";
-$plot= "";
+$id = 0;
+$name = "";
+$author = "";
+$date = "";
+$collection = "";
+$category ="";
+$color = "#b4dfe5";
+$plot = "";
 $update = false;
+$range = 0;
+
 
 $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
 if($mysqli->connect_error) {
@@ -18,10 +22,15 @@ if(isset($_POST['save'])){
     $date= $_POST['date'];
     $collection= $_POST['collection'];
     $plot= $_POST['plot'];
+    $category = $_POST['category'];
+    $color= $_POST['color'];
+    $range = $_POST['range'];
 
-    $mysqli->query("INSERT INTO shelf (name, author, date, collection, plot)
-                    VALUES ('$name', '$author', '$date', '$collection', '$plot')") or
+    $mysqli->query("INSERT INTO shelf (name, author, date, collection, plot, category, color, progress)
+                    VALUES ('$name', '$author', '$date', '$collection', '$plot', '$category', '$color', '$range')") or
                     die($mysqli->error);
+    
+    
 
     header("location: index.php");
 }
@@ -43,18 +52,24 @@ if(isset($_GET['edit'])){
     $date= $record['date'];
     $collection= $record['collection'];
     $plot= $record['plot'];
+    $category= $record['category'];
+    $color = $record['color'];
+    $range = $record['progress'];
 }
 
 if (isset($_POST['update'])) {
     $id = $_POST['id'];
     $name = $_POST['name'];
-    $author= $_POST['author'];
-    $date= $_POST['date'];
-    $collection= $_POST['collection'];
-    $plot= $_POST['plot'];
+    $author = $_POST['author'];
+    $date = $_POST['date'];
+    $collection = $_POST['collection'];
+    $plot = $_POST['plot'];
+    $category = $_POST['category'];
+    $color = $_POST['color'];
+    $range = $_POST['range'];
 
     $mysqli->query("UPDATE shelf
-    SET name='$name', author='$author', date='$date', collection='$collection', plot='$plot'
+    SET name='$name', author='$author', date='$date', collection='$collection', plot='$plot', category='$category', color='$color', progress='$range'
     WHERE id=$id") or die($mysqli->error);
 
     header("location: index.php");
